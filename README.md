8bed0ad331cd2592057248e3249d1b9a20145d82
Hrishav Kumar <33994932+hrishavkmr@users.noreply.github.com>
2020-10-26 15:45:42 +0530

    develop - add logic to generate search report  (#1)
    * add logic to save the results in csv
    * add .env file to store secrets
    
    Co-authored-by: Chetanya Kandhari <chetanya.kandhari@brightscout.com>

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # mattermost-search-analytics
+
+## Setup instructions
+* Install python 3.6+ and pip.
+* Run `pip3 install -r requirements.txt` in project directory to install the depedencies.
+* Create a file `.env` and paste the contents of `.env.sample` in it.
+    ```
+    cat .env.sample > .env
+    ```
+    * Set mattermost server URL as API_URL. 
+    * Set team id for which you want to perform the search as TEAM_ID. You can retrieve team id from the database.
+    * Set personal access token as ACCESS_TOKEN.
+        * You have to enable personal access tokens in `System Console > Integrations > Custom Integrations`.
+        * To create a personal access token, go to `Account Settings > Security > Personal Access Tokens` and click `Create New Token`.
+        * You can refer [here](https://docs.mattermost.com/developer/personal-access-tokens.html#personal-access-tokens) to know more about how to create personal access token.
+* Create a search query input file similar to `search.txt.sample`. 
+
+## Run script
+   * Use this command `python test.py <input-file> <output-file>` to generate a report.
+   * if no `<output-file>` is provided, then it will generate a report with filename `<input-file>-timestamp.csv`.
+   * example -<br> 
+       > `python search.py search.txt output.csv
+        `<br>
+        If everything is configured correctly. This command will generate a `output.csv` file and log the info similar to this.
+        ![alt-text](docs/images/img.png)
+        >
---
+++ search.py
@@ -0,0 +1,94 @@
+"""
+This module contains the code to generate a search report(in csv)
+for multiple mattermost search.
+"""
+import calendar
+import csv
+import json
+import logging
+import os
+import sys
+import time
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = os.getenv("API_URL")
+TEAM_ID = os.getenv("TEAM_ID")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+def save_to_csv(filename, fields, rows):
+    """
+    This function saves a csv file with given fields and rows.
+    """
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(fields)
+        csvwriter.writerows(rows)
+
+
+def test(args):
+    """
+    This function reads a search query file, perform search for each search query
+    and then saves the result metric to csv.
+    """
+    if len(args) == 0:
+        raise BaseException('filename is required')
+
+    url = "{}/api/v4/teams/{}/posts/search".format(
+        API_URL, TEAM_ID)
+    headers = {
+        'Authorization': 'Bearer {}'.format(ACCESS_TOKEN),
+    }
+
+    # field names
+    fields = ['search_text', 'count', 'time(in ms)']
+
+    # name of csv file
+    filename = ''
+    if len(args) == 2:
+        filename = args[1]
+    else:
+        filename = "{} - {}.csv".format(args[0], calendar.timegm(time.gmtime()))
+
+    input_file = open(args[0], "r")
+
+    results = []
+
+    min_response_time = 10000
+    max_response_time = 0
+    # traverse through all the search term
+    for term in input_file:
+        data = {
+            "terms": term,
+        }
+        # fetch search response and store response in results array.
+        response = requests.post(
+            url, headers=headers, data=json.dumps(data))
+        if response.status_code != 200:
+            raise BaseException(
+                "Error: recieved {} from URL with message:{}".format(
+                    response.status_code, response.text))
+        elapsed_seconds = response.elapsed.total_seconds()
+        results.append([term,
+                        len(response.json()['order']),
+                        elapsed_seconds * 1000])
+        min_response_time = min(min_response_time, elapsed_seconds)
+        max_response_time = max(max_response_time, elapsed_seconds)
+
+    # save results to csv
+    save_to_csv(filename, fields, results)
+
+    #log the minimum and maximum reponse time in ms
+    logging.info("Mininum response time(in ms): %f", min_response_time*1000)
+    logging.info("Maximum response time(in ms): %f", max_response_time*1000)
+
+if __name__ == "__main__":
+    try:
+        test(sys.argv[1:])
+    except BaseException as error:  # pylint: disable=broad-except
+        logging.error(error)
